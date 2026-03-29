bde22cd48908c6305321e52713e9fe083fd39ac4
Łukasz Zychal <lukasz.zychal.dev@gmail.com>
2026-03-29 20:15:50 +0200

    Update UserRegistrationService.php

--- 02-php-service-layer/service-layer-php-example/symfony/src/Service/UserRegistrationService.php
+++ 02-php-service-layer/service-layer-php-example/symfony/src/Service/UserRegistrationService.php
@@ -5,26 +5,47 @@ namespace App\Service;
 use App\DTO\UserRegistrationDTO;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-// use Symfony\Component\Mailer\MailerInterface; ...itd.
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserRegistrationService
 {
     public function __construct(
-        private EntityManagerInterface $entityManager
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
+        private MailerInterface $mailer
     ) {}
 
     public function registerUser(UserRegistrationDTO $dto): User
     {
-        // Here is the logic for creating the User object for Doctrine, 
-        // hashing the password and saving it to the database.
-        
         $user = new User();
         $user->setEmail($dto->email);
         $user->setName($dto->name);
-        // ...
         
+        // Assigning a default role
+        $user->setRoles(['ROLE_USER']);
+
+        // Hashing the password
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $dto->password
+        );
+        $user->setPassword($hashedPassword);
+
+        // Saving to the database
+        // In Doctrine, flush() implicitly handles the transaction and commit
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        // Sending an email OUTSIDE/AFTER the database transaction
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to($user->getEmail())
+            ->subject('Welcome to our app!')
+            ->text('Hello ' . $user->getName() . '! Your account has been successfully created.');
+
+        $this->mailer->send($email);
 
         return $user;
     }
